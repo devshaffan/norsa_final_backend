4abbede32493deecde82901aff52e5aa8407c6a4
devshaffan <devshaffan@gmail.com>
2021-12-12 18:36:30 +0500

    cloudinary commented

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,8 +1,8 @@
 
-const cloudinary = require("cloudinary").v2;
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'norsa',
-    api_key: process.env.CLOUDINARY_API_KEY || '666263827466723',
-    api_secret: process.env.CLOUDINARY_API_SECRET || '9PoAtT_Dy-GDp_2N_cmrrLfPLRc',
-});
-module.exports = cloudinary;
+// const cloudinary = require("cloudinary").v2;
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'norsa',
+//     api_key: process.env.CLOUDINARY_API_KEY || '666263827466723',
+//     api_secret: process.env.CLOUDINARY_API_SECRET || '9PoAtT_Dy-GDp_2N_cmrrLfPLRc',
+// });
+// module.exports = cloudinary;
